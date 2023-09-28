df7dd58af77ed221b7bc0fd5b99f1894dd7323aa
miin22 <141325340+miin22@users.noreply.github.com>
2023-09-28 11:01:51 -0300

    css

---
+++ cadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <form action="cadastro de upload.php" method="post"
+    enctype="multipart/form-data" >
+
+    <div>
+        <label>Imagem</label>
+        <input type="file" name="imagem" />
+</div>
+<div>
+    <input type="submit" value="upload" />
+</div>
+</h1>
+</body>
+</html>
